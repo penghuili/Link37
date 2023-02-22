dd26c926607a46ec44d781e27c3f4f88d611f406
Peng <1a84@proton.me>
2023-02-22 19:40:29 +0100

    fix: pitch

--- src/components/Pitch.js
+++ src/components/Pitch.js
@@ -2,7 +2,7 @@ import { Text } from 'grommet';
 import React from 'react';
 
 function Pitch() {
-  return <Text>Bookmark 2.0, encrypted.</Text>;
+  return <Text>Encrypted bookmarks.</Text>;
 }
 
 export default Pitch;
