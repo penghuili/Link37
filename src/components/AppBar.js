f914c5512d324e74bed9f08f7ca660b04f441818
Peng <1a84@proton.me>
2023-02-25 22:48:20 +0100

    fix: add hasBack

--- src/components/AppBar.js
+++ src/components/AppBar.js
@@ -3,8 +3,8 @@ import React from 'react';
 import logo from '../assets/logo.png';
 import BaseAppBar from '../shared/react/AppBar';
 
-function AppBar({ isLoading }) {
-  return <BaseAppBar logo={logo} isLoading={isLoading} />;
+function AppBar({ isLoading, hasBack }) {
+  return <BaseAppBar logo={logo} isLoading={isLoading} hasBack={hasBack} />;
 }
 
 export default AppBar;
